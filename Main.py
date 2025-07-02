e6869bed7410e1e840d95fa0fb57d4f07ba5a1c8
Goga_ua <tanjapavlenko14122011@gamil.com>
2025-07-02 12:59:28 +0200

    Exercise 1

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+class Book:
+    def __init__(self, title="", author="", pages=0, year=0):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.year = year
+
+    def __setattr__(self, key, value):
+        if key in ("title", "author") and not isinstance(value, str):
+            raise TypeError("Неверный тип присваиваемых данных.")
+        elif key in ("pages", "year") and not isinstance(value, int):
+            raise TypeError("Неверный тип присваиваемых данных.")
+        else:
+            object.__setattr__(self, key, value)
+
+
+book = Book("Python ООП", "Сергей Балакирев", 123, 2022)
