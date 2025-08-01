4a5d54a10d1a826ab23c3c686c3c20a967b8cebc
Goga_ua <tanjapavlenko14122011@gamil.com>
2025-08-01 17:58:23 +0200

    Double Linked List

--- Main.py
+++ Main.py
@@ -1,1 +1,43 @@
+class ObjList:
+    def __init__(self, data):
+        self.__data = data
+        self.__prev = None
+        self.__next = None
 
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_obj(self, obj):
+        """
+        Adds new object to the end of the list
+        :param obj: ObjList class ex.
+        :return: Doesn't return anything
+        """
+        ...
+
+    def remove_obj(self, indx):
+        """
+        Removes object from list by index
+        :param indx: element index
+        :return: Doesn't return anything
+        """
+        ...
+
+    def __len__(self, linked_lst):
+        """
+        Shows total of list
+        :param linked_lst:
+        :return: Returns length of list
+        """
+        ...
+
+    def linked_lst(self, indx):
+        """
+        Shows data from list object
+        :param indx: class obj index
+        :return: Returns __data
+        """
+        ...
