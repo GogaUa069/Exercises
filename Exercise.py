0c029f0a3366b6e57e59a38c9d99ebe1ac8ae6e6
Goga_ua <tanjapavlenko14122011@gamil.com>
2025-08-05 18:34:19 +0200

    Complex numbers

--- Exercise.py
+++ Exercise.py
@@ -1,17 +1,50 @@
-class Book:
-    def __init__(self, title="", author="", pages=0, year=0):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.year = year
+from math import sqrt
 
-    def __setattr__(self, key, value):
-        if key in ("title", "author") and not isinstance(value, str):
-            raise TypeError("Неверный тип присваиваемых данных.")
-        elif key in ("pages", "year") and not isinstance(value, int):
-            raise TypeError("Неверный тип присваиваемых данных.")
+
+class Complex:
+    """
+    Working with complex numbers
+    """
+    def __init__(self, real, img):
+        """
+        :param real: int/float. Real part of complex number.
+        :param img: int/float. Imaginary part of complex number.
+        """
+        self.__real = real
+        self.__img = img
+
+    @property
+    def real(self):
+        return self.__real
+
+    @real.setter
+    def real(self, value):
+        if type(value) in (int, float):
+            self.__real = value
         else:
-            object.__setattr__(self, key, value)
+            raise ValueError("Неверный тип данных.")
+
+    @property
+    def img(self):
+        return self.__img
+
+    @img.setter
+    def img(self, value):
+        if type(value) in (int, float):
+            self.__img = value
+        else:
+            raise ValueError("Неверный тип данных.")
+
+    def __abs__(self):
+        """
+        :return: The square root of the sum of the squares of the real and imaginary parts of a complex number.
+        """
+        return sqrt(self.real**2 + self.__img**2)
 
 
-book = Book("Python ООП", "Сергей Балакирев", 123, 2022)
+# EX. <<<
+#cmp = Complex(7, 8)
+#cmp.real = 3
+#cmp.img = 4
+#c_abs = abs(cmp)
+#print(cmp.real, cmp.img, c_abs)
