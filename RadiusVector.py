d80caa49dc148b2be12165c974d0c4f9f6a23372
Goga_ua <tanjapavlenko14122011@gamil.com>
2025-08-07 11:56:22 +0200

    RadiusVector2

--- RadiusVector.py
+++ RadiusVector.py
@@ -3,31 +3,24 @@ from math import sqrt
 
 class RadiusVector:
     def __init__(self, *args):
-        self.coord_length = len(args)
-        self.coord_tuple = tuple()
-
-        if self.coord_length == 1:
-            self.coord_tuple = (0, )*args[0]
+        if len(args) == 1:
+            self.coord_list = [0]*args[0]
+        else:
+            self.coord_list = list(args)
 
     def set_coords(self, *args):
-        self.coord_tuple = tuple(args)
+        for indx, arg in enumerate(args):
+            if indx < len(self.coord_list):
+                self.coord_list[indx] = arg
+            else:
+                self.coord_list.append(arg)
 
     def get_coords(self):
-        return self.coord_tuple
+        return tuple(self.coord_list)
 
     def __len__(self):
-        return len(self.coord_tuple)
+        return len(self.coord_list)
 
     def __abs__(self):
-        square_root_sum_coords = sum([coord**2 for coord in self.coord_tuple])
-        return sqrt(square_root_sum_coords)
-
-
-#vector3D = RadiusVector(5)
-#vector3D.set_coords(10, 20, 30)
-
-#print("Coords:", vector3D.get_coords())
-#print("Length:", len(vector3D))
-#print("Abs:", abs(vector3D))
-#a, b, c = vector3D.get_coords()
-#print("a, b, c:", a, b, c)
+        square_coords = sum([coord**2 for coord in self.coord_list])
+        return sqrt(square_coords)
