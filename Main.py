b9b6483372f795ee8bd1a5ad66d9f002aefa4daa
Goga_ua <pavlenkoegor1412@gmail.com>
2025-09-14 16:15:39 +0200

    List - List

--- Main.py
+++ Main.py
@@ -1,43 +1,22 @@
-class ObjList:
-    def __init__(self, data):
-        self.__data = data
-        self.__prev = None
-        self.__next = None
+class NewList:
+    def __init__(self, *args):
+        self.__data_list = list(*args)
+
+    def get_list(self):
+        return self.__data_list
+
+    def __sub__(self, other):
+        other_list = other.get_list() if isinstance(other, NewList) else other
+        for other_data in other_list:
+            for data in self.__data_list:
+                if other_data is data:
+                    self.__data_list.remove(data)
+                    other_list.remove(data)
+        return NewList(self.__data_list)
 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+list1 = NewList([0, 1, 2, 3])
+list2 = NewList([0, False, True])
 
-    def add_obj(self, obj):
-        """
-        Adds new object to the end of the list
-        :param obj: ObjList class ex.
-        :return: Doesn't return anything
-        """
-        ...
-
-    def remove_obj(self, indx):
-        """
-        Removes object from list by index
-        :param indx: element index
-        :return: Doesn't return anything
-        """
-        ...
-
-    def __len__(self, linked_lst):
-        """
-        Shows total of list
-        :param linked_lst:
-        :return: Returns length of list
-        """
-        ...
-
-    def linked_lst(self, indx):
-        """
-        Shows data from list object
-        :param indx: class obj index
-        :return: Returns __data
-        """
-        ...
+res1 = list1 - list2
+print(res1.get_list())  # [1, 2, 3]
