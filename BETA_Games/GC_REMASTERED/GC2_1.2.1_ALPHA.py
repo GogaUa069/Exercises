3200ee9ef2934cbc34d75d0ae24dc93268bb6da6
Goga_ua <pavlenkoegor1412@gmail.com>
2025-12-09 16:53:40 +0100

    Guess Code 2.0 v1.2.1 ALPHA

--- BETA_Games/GC_REMASTERED/GC2_1.2.1_ALPHA.py
+++ BETA_Games/GC_REMASTERED/GC2_1.2.1_ALPHA.py
@@ -1,4 +1,4 @@
-# Guess Code 2 V 1.2.0 ALPHA
+# Guess Code 2 V 1.2.1 ALPHA
 
 # Add CONTINUE MENU
 # Add channels for soundtracks
@@ -188,11 +188,42 @@ class GameIntro:
     def __call__(self):
         intro_soundtrack()
         self.get_my_accounts()
-        self.show_game_banner("Guess Code 2.0\nv 1.2.0 ALPHA")
+        self.show_game_banner("Guess Code 2.0\n v 1.2.1 ALPHA")
         self.loading()
 
 
 game_intro = GameIntro()
+
+
+class Option:
+    def __init__(self, name: str, func):
+        self.NAME = name
+        self.FUNC = func
+
+    def __call__(self):
+        self.FUNC()
+
+
+class BorderOption:
+    def __init__(self, lvl: str, name: str, lvl_type: str, descr: str):
+        self.LVL = lvl
+        self.NAME = name
+        self.LVL_TYPE = lvl_type
+        self.DESCR = descr
+        self.DATA = [self.LVL, self.NAME, self.LVL_TYPE, self.DESCR]
+
+
+basic_game_border = BorderOption("1", "BASIC", system.LVL_TYPES["COMMON"], "Infinite lives. Time-free mode.")
+average_game_border = BorderOption("2", "AVERAGE", system.LVL_TYPES["COMMON"], "Finite lives. Time-free mode.")
+advanced_game_border = BorderOption("3", "ADVANCED", system.LVL_TYPES["COMMON"], "Finite lives. Countdown active.")
+wild_game_border = BorderOption("4", "WILD", system.LVL_TYPES["EPIC"], "Lives and time are randomized.")
+custom_game_border = BorderOption("5", "CUSTOM", system.LVL_TYPES["EPIC"], "Lives and time are under your control.")
+adventure_game_border = BorderOption("6", "ADVENTURE", system.LVL_TYPES["LEGENDARY"],
+                                     "Beat AVERAGE, ADVANCED, and WILD levels in a single run and get a prize!")
+leave_border = BorderOption("7",  "MAIN MENU", "---------", "Back to Main Menu")
+
+level_tup = (basic_game_border, average_game_border, advanced_game_border,
+             wild_game_border, custom_game_border, adventure_game_border, leave_border)
 
 
 class MenuPattern:
@@ -223,22 +254,30 @@ class MenuPattern:
             self.check_answer(input(system.INPUT))
 
 
-class Option:
-    def __init__(self, name: str, func):
-        self.NAME = name
-        self.FUNC = func
+class BorderPattern:
+    HEADERS = ["LVL", "NAME", "TYPE", "DESCRIPTION"]
+
+    def __init__(self, levels: tuple):
+        self.LEVELS = levels
+        self.LONGEST_LENGTH_LIST = list()
+        self.DATA = [*self.HEADERS] + [lvl.DATA for lvl in self.LEVELS]
+        self.set_the_longest_data_length()
+
+    def set_the_longest_data_length(self):
+        for items in zip(*self.DATA):
+            the_longest_item = max(len(item) for item in items)
+            self.LONGEST_LENGTH_LIST.append(the_longest_item)
 
     def __call__(self):
-        self.FUNC()
+        for row in self.DATA:
+            line = ""
+            for indx, item in enumerate(row):
+                line += "| " + item.ljust(self.LONGEST_LENGTH_LIST[indx]) + " "
+            line += "|"
+            print(line)
 
 
-class BorderOption:
-    def __init__(self, lvl: str, name: str, lvl_type: str, descr: str):
-        self.LVL = lvl
-        self.NAME = name
-        self.LVL_TYPE = lvl_type
-        self.DESCR = descr
-        self.DATA = [self.LVL, self.NAME, self.LVL_TYPE, self.DESCR]
+game_border = BorderPattern(level_tup)
 
 
 class Credits:
