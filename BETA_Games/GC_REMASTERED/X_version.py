92169312daf733b6298f20c3dda4e4062c8c2cd7
Goga_ua <pavlenkoegor1412@gmail.com>
2025-12-10 22:24:36 +0100

    GC_Xv Don't change anything

---
+++ BETA_Games/GC_REMASTERED/X_version.py
@@ -0,0 +1,233 @@
+# X v1.0.0
+
+# Add CONTINUE MENU
+# Add channels for soundtracks
+
+# DO SMTH: (funcs)
+# goodbye_func
+
+# ***************************************************************************************************
+
+from random import randint, choice
+from time import sleep
+import shutil
+import re
+import sys
+import os
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+
+from tqdm import tqdm
+import colorama
+import pyfiglet
+import pygame
+
+colorama.init()
+pygame.init()
+pygame.mixer.init()
+
+
+class SystemPrompt:
+    COLORS = [attr for attr in dir(colorama.Fore) if not attr.startswith("_")]
+
+    def __init__(self):
+        self.INPUT = self.communicate("<<< ", "LIGHTWHITE_EX")
+        self.RULES = (self.communicate("Press ENTER to continue\n","LIGHTRED_EX"),
+                      self.communicate(">>> Rules:","LIGHTRED_EX"),
+                      self.communicate(">>> Find the number the computer guessed.","BLUE"),
+                      self.communicate(">>> 1. Select a range (e.g. 5-200).","BLUE"),
+                      self.communicate(">>> 2. Start guessing.","BLUE"),
+                      self.communicate(">>> Your turn!","LIGHTRED_EX"))
+        self.LVL_TYPES = {"COMMON": self.communicate("COMMON", "LIGHTBLUE_EX"),
+                          "EPIC": self.communicate("EPIC", "LIGHTMAGENTA_EX"),
+                          "LEGENDARY": self.communicate("LEGENDARY", "LIGHTYELLOW_EX")}
+
+    @staticmethod
+    def del_ascii(text: str):
+        ansi_escape = re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]")
+        return ansi_escape.sub("", text)
+
+    @staticmethod
+    def resource_path(relative_path: str):
+        base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(base_path, relative_path)
+
+    @staticmethod
+    def communicate(text=None, color=None, is_error=False, is_bold=False, is_underlined=False):
+        if text is None and color is None:
+            return None
+        elif is_error:
+            text = f"ERROR: {text}"
+            color = "LIGHTRED_EX"
+
+        colored_text = getattr(colorama.Fore, color) + text + colorama.Style.RESET_ALL
+        if is_bold:
+            colored_text = colorama.Style.BRIGHT + colored_text
+        if is_underlined:
+            colored_text = "\033[4m" + colored_text + "\033[0m"
+        return colored_text
+
+    def get_ascii_text(self, text: str, color="LIGHTBLUE_EX", font="standard"):
+        f = pyfiglet.Figlet(font=font)
+        ascii_text = f.renderText(text)
+        for line in ascii_text.splitlines():
+            print(self.communicate(line, color, is_bold=True))
+            sleep(0.25)
+
+    def get_text_by_let(self, text: str):
+        for char in text:
+            sleep(0.21)
+            print(self.communicate(char, "LIGHTWHITE_EX"), end="")
+
+    def get_rules(self):
+        for line in self.RULES:
+            print(line, end="")
+            input()
+
+
+system = SystemPrompt()
+
+
+class Soundtrack:
+    def __init__(self, name: str, repeats=1):
+        self.NAME = name
+        self.REPEATS = repeats
+
+    @staticmethod
+    def turn_off_soundtrack():
+        pygame.mixer.music.stop()
+        print(system.communicate("Music if turned Off", "LIGHTRED_EX"))
+
+    def __call__(self):
+        pygame.mixer.music.load(f"Soundtracks/{self.NAME}")
+        pygame.mixer.music.play(self.REPEATS)
+
+
+intro_soundtrack = Soundtrack("IntroSoundtrack.wav")
+credits_soundtrack = Soundtrack("CreditsSoundtrack.wav", -1)
+jazz1 = Soundtrack("Jazz1.wav", -1)
+jazz2 = Soundtrack("Jazz2.mp3", -1)
+jazz3 = Soundtrack("Jazz3.wav", -1)
+
+
+class SoundPrompt:
+    YOU_WIN_SOUND = pygame.mixer.Sound("Sounds/YouWinSound.wav")
+    YOU_LOST_SOUND = pygame.mixer.Sound("Sounds/YouLostSound.wav")
+    ABLE_TO_PLAY = True
+
+    def __call__(self):
+        self.ABLE_TO_PLAY = not self.ABLE_TO_PLAY
+        match self.ABLE_TO_PLAY:
+            case True:
+                self.YOU_WIN_SOUND.play()
+                print(system.communicate("Sound is turned ON", "LIGHTGREEN_EX"))
+            case False:
+                self.YOU_LOST_SOUND.play()
+                print(system.communicate("Sound is turned OFF", "LIGHTRED_EX"))
+
+
+toggle_sound = SoundPrompt()
+
+
+class WinStreakController:
+    ENCODING = "utf-8"
+    RESOURCE_NAME = SAVE_NAME = "Utils/WIN_STREAK_FILE"
+
+    def __init__(self):
+        self.SAVE_PATH = os.path.join(os.getcwd(), self.SAVE_NAME)
+        self._ensure_writable_copy()
+
+    @staticmethod
+    def _resource_path(relative_path: str):
+        base_path = getattr(sys, "_MEIPASS", os.path.dirname(__file__))
+        return os.path.join(base_path, relative_path)
+
+    def _ensure_writable_copy(self):
+        if not os.path.exists(self.SAVE_PATH):
+            try:
+                shutil.copyfile(self._resource_path(self.RESOURCE_NAME), self.SAVE_PATH)
+            except Exception:
+                with open(self.SAVE_PATH, "w", encoding=self.ENCODING) as file:
+                    file.write("0")
+
+    def get_win_streak(self, is_communicate=False):
+        with open(self.SAVE_PATH, "r", encoding=self.ENCODING) as file:
+            streak = file.read()
+            if not is_communicate:
+                return int(streak)
+            else:
+                return f"Your win streak: {streak}"
+
+    def __call__(self, is_defeat=False):
+        new_streak = 0 if is_defeat else self.get_win_streak() + 1
+        with open(self.SAVE_PATH, "w", encoding=self.ENCODING) as file:
+            file.write(str(new_streak))
+
+
+win_streak_controller = WinStreakController()
+
+
+class GameIntro:
+    GitHub_URL = "https://github.com/GogaUa069"
+    Instagram_URL = "https://www.instagram.com/gogaua096/"
+
+    def get_my_accounts(self):
+        print(system.communicate(f"{self.Instagram_URL} - Follow me on Instagram :)", "CYAN"))
+        print(system.communicate(f"{self.GitHub_URL} - Check my GitHub\n", "CYAN"))
+
+    @staticmethod
+    def loading():
+        with tqdm(total=100) as pbar:
+            for i in range(100):
+                color = choice([c for c in system.COLORS if c != "BLACK"])
+                pbar.set_description_str(system.communicate("Loading", color))
+                pbar.update(1)
+                sleep(0.04)
+
+    @staticmethod
+    def show_game_banner(banner: str):
+        sleep(0.5)
+        print(system.communicate("Welcome to:", "CYAN", is_bold=True))
+        sleep(1.5)
+        system.get_ascii_text(banner)
+        print(system.communicate("Made by GogaUa\n", "WHITE", is_bold=True))
+        sleep(1)
+
+    def __call__(self):
+        intro_soundtrack()
+        self.get_my_accounts()
+        self.show_game_banner("Guess Code 2.0\n X version")
+        self.loading()
+
+
+game_intro = GameIntro()
+
+
+class Credits:
+    HEADER = "CREDITS:"
+
+    def __init__(self, *args):
+        self.CREDITS = list(args)
+
+    def get_credits(self):
+        for indx, credit in enumerate(self.CREDITS):
+            text = f"{indx+1}. {credit}"
+            system.get_text_by_let(text)
+            print()
+        sleep(1)
+        print()
+
+    @staticmethod
+    def leave_credits():
+        print(system.communicate("All audio was taken from freesound.org", "LIGHTWHITE_EX", is_underlined=True))
+        input(system.communicate(">>> Press ENTER to leave", "LIGHTRED_EX"))
+        pygame.mixer.music.stop()
+
+    def __call__(self):
+        credits_soundtrack()
+        system.get_ascii_text(self.HEADER)
+        self.get_credits()
+        self.leave_credits()
+
+
+credits_ = Credits("Egor Pavlenko - CEO")
