c9ab99ce5298e99c5eafacfb989a145bca450491
Goga_ua <pavlenkoegor1412@gmail.com>
2025-12-13 21:54:29 +0100

    Guess Code 1 v6.0.0 Remastered

--- BETA_Games/GC_REMASTERED/GC1_6.0.0_REMASTERED.py
+++ BETA_Games/GC_REMASTERED/GC1_6.0.0_REMASTERED.py
@@ -1,8 +1,7 @@
-# X v1.0.0
+# Guess Code 2.0 REMASTERED BETA
 
 # Add CONTINUE MENU
 # Add channels for soundtracks
-# Add goodbye func
 
 # ***************************************************************************************************
 
@@ -79,6 +78,10 @@ class SystemPrompt:
             colored_text = "\033[4m" + colored_text + "\033[0m"
         return colored_text
 
+    def quit_game(self):
+        print(self.communicate("Goodbye :)", "LIGHTCYAN_EX"))
+        sleep(0.5)
+
     def get_ascii_text(self, text: str, color="LIGHTBLUE_EX", font="standard"):
         """
         Prints text in ASCII format.
@@ -142,8 +145,8 @@ class SoundPrompt:
     """
     Works with sounds.
     """
-    YOU_WIN_SOUND = pygame.mixer.Sound("Sounds/YouWinSound.wav")
-    YOU_LOST_SOUND = pygame.mixer.Sound("Sounds/YouLostSound.wav")
+    YOU_WIN_SOUND = pygame.mixer.Sound("Sounds/YouWinSound.wav")  # MAXTIME = 2350
+    YOU_LOST_SOUND = pygame.mixer.Sound("Sounds/YouLostSound.wav")  # MAXTIME = 2500
     ABLE_TO_PLAY = True
 
     def __call__(self):
@@ -153,10 +156,10 @@ class SoundPrompt:
         self.ABLE_TO_PLAY = not self.ABLE_TO_PLAY
         match self.ABLE_TO_PLAY:
             case True:
-                self.YOU_WIN_SOUND.play()
+                self.YOU_WIN_SOUND.play(maxtime=2350)
                 print(system.communicate("Sound is turned ON", "LIGHTGREEN_EX"))
             case False:
-                self.YOU_LOST_SOUND.play()
+                self.YOU_LOST_SOUND.play(maxtime=2500)
                 print(system.communicate("Sound is turned OFF", "LIGHTRED_EX"))
 
 
@@ -244,7 +247,7 @@ class GameIntro:
     def __call__(self):
         intro_soundtrack()
         self.get_my_accounts()
-        self.show_game_banner("Guess Code 2.0\n X version")
+        self.show_game_banner("Guess Code v6.0.0\nRemastered")
         self.loading()
 
 
@@ -279,3 +282,124 @@ class Credits:
 
 
 credits_ = Credits("Egor Pavlenko - CEO")
+
+
+class Option:
+    def __init__(self, name: str, func):
+        self.NAME = name
+        self.FUNC = func
+
+    def __call__(self):
+        self.FUNC()
+
+
+class BorderOption:
+    def __init__(self, lvl: str, name: str, lvl_type: str, descr: str):
+        self.LVL = lvl
+        self.NAME = name
+        self.LVL_TYPE = lvl_type
+        self.DESCR = descr
+        self.DATA = [self.LVL, self.NAME, self.LVL_TYPE, self.DESCR]
+
+
+LEVELS = [
+    BorderOption("1", "BASIC", system.LVL_TYPES["COMMON"], "Infinite lives. Time-free mode."),
+    BorderOption("2", "AVERAGE", system.LVL_TYPES["COMMON"], "Finite lives. Time-free mode."),
+    BorderOption("3", "ADVANCED", system.LVL_TYPES["COMMON"], "Finite lives. Countdown active."),
+    BorderOption("4", "WILD", system.LVL_TYPES["EPIC"], "Lives and time are randomized."),
+    BorderOption("5", "CUSTOM", system.LVL_TYPES["EPIC"], "Lives and time are under your control."),
+    BorderOption("6", "ADVENTURE",  system.LVL_TYPES["LEGENDARY"],"Beat AVERAGE, ADVANCED, and WILD levels in a single run and get a prize!"),
+    BorderOption("7", "MAIN MENU", "---------", "Back to Main Menu")]
+
+
+class MenuPattern:
+    def __init__(self, header: str, options: tuple, border=None):
+        self.HEADER, self.OPTIONS, self.BORDER = header, options, border
+
+    def show_menu(self):
+        if self.BORDER is None:
+            print(system.communicate(f"\n>>> {self.HEADER}", "LIGHTRED_EX"))
+            for i, opt in enumerate(self.OPTIONS, 1):
+                print(system.communicate(f"{i}. {opt.NAME}", "LIGHTBLUE_EX"))
+        else:
+            self.BORDER()
+
+    def check_answer(self, answer: str):
+        for indx, option in enumerate(self.OPTIONS, 1):
+            if answer.upper() in (option.NAME.upper(), str(indx)):
+                option()
+                return
+        print(system.communicate("Select one of the options shown above!", is_error=True))
+
+    def __call__(self):
+        answer = str
+        while answer != str(len(self.OPTIONS)):
+            self.show_menu()
+            answer = input(system.INPUT)
+            self.check_answer(answer)
+
+
+class BorderPattern:
+    HEADERS = ["LVL", "NAME", "TYPE", "DESCRIPTION"]
+
+    def __init__(self, levels: list):
+        self.DATA = [self.HEADERS] + [lvl.DATA for lvl in levels]
+        self.WIDTHS = [max(len(system.del_ascii(item)) for item in col) for col in zip(*self.DATA)]
+
+    def __call__(self):
+        for row in self.DATA:
+            line = "".join(f"| {item.ljust(self.WIDTHS[i]+1)}" for i, item in enumerate(row)) + "|"
+            print(line)
+
+
+game_border = BorderPattern(LEVELS)
+
+
+# SOUNDTRACKS
+jazz1_option = Option("Jazz1", jazz1)
+jazz2_option = Option("Jazz2", jazz2)
+jazz3_option = Option("Jazz3", jazz3)
+turn_off_soundtrack_option = Option("Turn Off", Soundtrack.turn_off_soundtrack)
+from_soundtracks_to_audio = Option("Back - Audio", system.communicate)
+
+soundtracks_options = (jazz1_option, jazz2_option, jazz3_option, turn_off_soundtrack_option, from_soundtracks_to_audio)
+soundtracks_menu = MenuPattern("Soundtracks", soundtracks_options)
+
+# SOUNDS
+toggle_sound_option = Option("Turn Off/On", toggle_sound)
+from_sounds_to_audio = Option("Back - Audio", system.communicate)
+
+sounds_options = (toggle_sound_option, from_sounds_to_audio)
+sounds_menu = MenuPattern("Sounds", sounds_options)
+
+# AUDIO
+soundtracks_option = Option("Soundtracks", soundtracks_menu)
+sounds_option = Option("Sounds", sounds_menu)
+from_audio_to_settings = Option("Back - Settings", system.communicate)
+
+audio_options = (soundtracks_option, sounds_option, from_audio_to_settings)
+audio_menu = MenuPattern("Audio", audio_options)
+
+# SETTINGS
+rules_option = Option("Rules", system.get_rules)
+audio_option = Option("Audio", audio_menu)
+credits_option = Option("Credits", credits_)
+from_settings_to_main_menu = Option("Back - Main Menu", system.communicate)
+
+settings_options = (rules_option, audio_option, credits_option, from_settings_to_main_menu)
+settings_menu = MenuPattern("Settings", settings_options)
+
+# MAIN MENU
+play_option = Option("Play", system.communicate)
+settings_option = Option("Settings", settings_menu)
+quit_option = Option("Quit", system.quit_game)
+
+main_menu_options = (play_option, settings_option, quit_option)
+main_menu = MenuPattern("Main Menu", main_menu_options)
+
+
+def game():
+    game_intro()
+    main_menu()
+
+game()
