858a1c641d2e4ad5c28e7cfe9d76e1f9cc39614b
Goga_ua <pavlenkoegor1412@gmail.com>
2025-12-25 23:10:20 +0100

    Trading Simulator. remastering. SystemConfig

--- BETA_Projects/TradingSimulator/CodeFiles/SystemConfig.py
+++ BETA_Projects/TradingSimulator/CodeFiles/SystemConfig.py
@@ -10,8 +10,8 @@ class SystemConfig:
     ACCOUNT_DIRECTORY_PATH = "TradingSimulator/Accounts/"
     PRIVACY_POLICY_FILE = "../Utils/PrivacyPolicy"
     FILE_ENCODING = "utf-8"
-    PATTERNS = {"full_name": re.compile(r"^[a-zA-Z-' ]{9}$"),
-                "email": re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")}
+    # PATTERNS = {"full_name": re.compile(r"^[a-zA-Z-' ]{9}$"),
+    #             "email": re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")}
 
     def __new__(cls, *args, **kwargs):
         if cls.__ex_counter == 1:
@@ -27,8 +27,8 @@ class SystemConfig:
         text = f">>> {text}" if is_system else text
         return getattr(Fore, color) + text + Style.RESET_ALL
 
-    def validator(self, value: str, pattern: str) -> bool:
-        return self.PATTERNS[pattern].fullmatch(value) is not None
+    # def validator(self, value: str, pattern: str) -> bool:
+    #     return self.PATTERNS[pattern].fullmatch(value) is not None
 
 
 system = SystemConfig()
