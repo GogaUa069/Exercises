e53f96f792f88f4b107305d88aa7d111a9182d05
Goga_ua <pavlenkoegor1412@gmail.com>
2026-02-05 22:05:03 +0100

    removed underlined text

--- BETA_Games/GlobalWars/FILES/MAIN_FILES/GUI_CODE.py
+++ BETA_Games/GlobalWars/FILES/MAIN_FILES/GUI_CODE.py
@@ -1,23 +1,22 @@
 from SYSTEM import system
-from BORDER import fog_border
 
 # HEADQUARTER
 operations_option = system.Option("operations", system.coming_soon)  # ***
 army_option = system.Option("army", system.coming_soon)  # ***
 logistics = system.Option("logistics", system.coming_soon)  # ***
-from_hq_to_main_menu = system.Option(system.communicate("LEAVE BATTLE", is_underlined=True), system.pass_func)
+from_hq_to_main_menu = system.Option("leave battle", system.pass_func)
 
 hq_options = (operations_option, army_option, logistics, from_hq_to_main_menu)
-hq_menu = system.Menu("HEADQUARTER", hq_options)
+hq_menu = system.Menu("headquarter", hq_options)
 
 # BATTLE
 frontline_option = system.Option("frontline", hq_menu)
 sectors_option = system.Option("sectors", hq_menu)
 citadel_option = system.Option("citadel", hq_menu)
-from_battle_to_main_menu = system.Option(system.communicate("BACK - MAIN MENU", is_underlined=True), system.pass_func)
+from_battle_to_main_menu = system.Option("back - main menu", system.pass_func)
 
 battle_options = (frontline_option, sectors_option, citadel_option, from_battle_to_main_menu)
-battle_menu = system.Menu("BATTLE MODES", battle_options)
+battle_menu = system.Menu("battle modes", battle_options)
 
 # MAIN MENU
 battle_option = system.Option("battle modes", battle_menu)
