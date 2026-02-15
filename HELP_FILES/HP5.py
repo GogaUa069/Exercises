ff706a25f3e1995f804d0435a680d3aa0efe6482
Goga_ua <pavlenkoegor1412@gmail.com>
2026-02-15 16:14:05 +0100

    learning german words. Remastered. beta

--- HELP_FILES/HP5.py
+++ HELP_FILES/HP5.py
@@ -39,7 +39,7 @@ class RecordData:
 
     def save_new_data(self, data):
         with open("the_best_time.yaml", "w", encoding=self.encoding) as file:
-            yaml.dump(data, file)
+            yaml.dump(data, file, sort_keys=False)
 
 
 record_data = RecordData()
@@ -47,10 +47,10 @@ record_data = RecordData()
 
 class WordsChecker:
     def __init__(self):
-        self.bad_answers_limit_for_record = 2.0
+        self.bad_answers_limit_for_record = 4
         self.good_answers_counter = self.bad_answers_counter = self.round_time = self.difference_time = self.bad_answers_difference = 0.0
         self.start_time = self.stop_time = 0
-        self.current_answer_timing = list()
+        self.current_answer_timing = dict()
 
     @staticmethod
     def __set_num_colors(num):
@@ -105,7 +105,7 @@ class WordsChecker:
             print(communicate(f"Różnica czasów: {self.__set_num_colors(self.difference_time)}", "LIGHTCYAN_EX"))
 
     def results(self):
-        print(communicate("WYNIKI", "LIGHTWHITE_EX"))
+        print(communicate("\nWYNIKI", "LIGHTWHITE_EX"))
         print(communicate(f"Punkty: {self.good_answers_counter}/{float(words.words_len)}", "LIGHTWHITE_EX"))
         print(communicate(f"Czas: {self.__get_time_in_seconds()} / {self.__get_time_in_minutes()}", "LIGHTWHITE_EX"))
         print(communicate(f"Błędów zrobiono: {self.bad_answers_counter}", "LIGHTWHITE_EX"))
@@ -115,21 +115,22 @@ class WordsChecker:
         is_bad = bad_counter = 0
         correct_answer = word[1][part_order]
         while True:
-            answer = input(communicate(f"{part}: ({bad_counter+1}/3)", "LIGHTWHITE_EX"))
+            answer = input(communicate(f"{part} ({bad_counter+1}/3): ", "LIGHTWHITE_EX"))
             if answer == correct_answer:
-                print(communicate("Poprawna odpowiedź!", "LIGHTGREEN_EX"))
+                print(communicate("Poprawna odpowiedź!\n", "LIGHTGREEN_EX"))
                 break
             else:
-                print(communicate("Błędna odpowiedź!", "LIGHTRED_EX"))
+                print(communicate("Błędna odpowiedź!\n", "LIGHTRED_EX"))
                 bad_counter += 1
                 if bad_counter > 1:
-                    self.bad_answers_counter += 1
                     is_bad = True
                 if bad_counter == 3:
-                    print(communicate(f"Poprawna odpowiedź: {communicate(correct_answer, "LIGHTWHITE_EX")}", "LIGHTRED_EX"))
+                    print(communicate(f"Poprawna odpowiedź: {communicate(correct_answer, "LIGHTWHITE_EX")}\n", "LIGHTRED_EX"))
                     break
         if not is_bad:
             self.good_answers_counter += 0.5
+        else:
+            self.bad_answers_counter += 1
 
     def set_timing_data(self, start_time_for_answer, stop_time_for_answer, order):
         time_for_answer = round(stop_time_for_answer - start_time_for_answer, 3)
@@ -137,7 +138,7 @@ class WordsChecker:
         difference_time_for_answer = self.__set_num_colors(round(time_for_answer - record_data.answers_timing[order]["time_for_answer"], 3))
         difference_time_since_start = self.__set_num_colors(round(time_since_start - record_data.answers_timing[order]["time_since_start"], 3))
 
-        self.current_answer_timing.append({order: {"time_for_answer": time_for_answer, "time_since_start": time_since_start}})
+        self.current_answer_timing[order] = {"time_for_answer": time_for_answer, "time_since_start": time_since_start}
 
         print(communicate(f"Czas na odpowiedź: {time_for_answer}", "LIGHTWHITE_EX"))
         print(communicate(f"Czas od początku: {time_since_start}", "LIGHTWHITE_EX"))
@@ -146,7 +147,7 @@ class WordsChecker:
 
     def get_words(self):
         for order, word in enumerate(words.words, 1):
-            print(communicate(f"{order}/{words.words_len}. {word[0]}:", "CYAN"))
+            print(communicate(f"\n{order}/{words.words_len}. {word[0]}:", "CYAN"))
             start_time_for_answer = time.time()
             self.validate_answer(word, 0, "bezokolicznik")
             self.validate_answer(word, 1, "imiesłów")
