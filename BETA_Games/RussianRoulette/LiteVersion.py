3dbbb7c4e10089d77a3002a38aa7645b9c49ccf6
Goga_ua <pavlenkoegor1412@gmail.com>
2026-03-08 20:15:34 +0100

    more lore. idk.

--- BETA_Games/RussianRoulette/LiteVersion.py
+++ BETA_Games/RussianRoulette/LiteVersion.py
@@ -2,9 +2,22 @@ import os
 from random import shuffle, randint
 from time import sleep
 from colorama import Fore, Style
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+import pygame
+
+pygame.init()
 
 
-def communicate(text, color):
+def play_audio(name="texting.wav", delay=1000):
+    pygame.mixer.music.load(rf"audio/{name}")
+    pygame.mixer.music.play()
+    pygame.time.delay(delay)
+    pygame.mixer.music.stop()
+
+
+def communicate(text, color="LIGHTRED_EX", is_man=True):
+    if is_man:
+        text = f"[noname]: {text}"
     return getattr(Fore, color) + text + Style.RESET_ALL
 
 
@@ -26,29 +39,42 @@ class Game:
     def player_dead(self):
         self.clear_cmd()
         sleep(3)
-        print(communicate("He's dead...", "LIGHTRED_EX"))
+        print(communicate("He's dead..."))
+        play_audio()
         sleep(3)
-        print(communicate("Come on, we need to dispose of him.", "LIGHTRED_EX"))
+        print(communicate("Come on, we need to dispose of him."))
+        play_audio()
         sleep(3)
         self.clear_cmd()
 
+    def player_won(self):
+        print(communicate("Oops. Dealer is out..."))
+        play_audio()
+        sleep(3)
+        self.clear_cmd()
+        sleep(3)
+        print(communicate("An eye for an eye..."))
+        play_audio()
+        sleep(3)
+
     def player_shot(self):
         print(f"\nChance to die: {self.cylinder.count(1)}/{len(self.cylinder)}")
-        input(communicate("Press ENTER to shot", "CYAN"))
+        input(communicate("Press ENTER to shot", "CYAN", False))
         bullet = self.cylinder[0]
         del self.cylinder[0]
         if bullet:
             self.player = False
             self.player_dead()
         else:
-            print(communicate("You alive.", "LIGHTGREEN_EX"))
+            print(communicate("You alive."))
+            play_audio()
             shuffle(self.cylinder)
 
     def player_move(self):
         self.player_shot()
         while self.player:
             print(f"\nChance to die: {self.cylinder.count(1)}/{len(self.cylinder)}")
-            print(communicate("Shot or stay?", "CYAN"))
+            print(communicate("Shot or stay?", "CYAN", False))
             answer = input("<<< ")
             match answer:
                 case "1" | "shot":
@@ -56,20 +82,20 @@ class Game:
                 case "2" | "stay":
                     break
                 case _:
-                    print(communicate("Select one of the options shown above!", "LIGHTRED_EX"))
+                    print(communicate("Select one of the options shown above!"))
 
     def dealer_move(self):
         print(f"\nChance to win: {self.cylinder.count(1)}/{len(self.cylinder)}")
-        print(communicate("Dealers move...", "CYAN"))
+        print(communicate("Dealers move...", "CYAN", False))
         sleep(randint(1, 3))
         bullet = self.cylinder[0]
         del self.cylinder[0]
         if bullet:
             self.dealer = False
-            print(communicate("Dealer is out...", "LIGHTGREEN_EX"))
-            input(communicate("\nPress ENTER to leave", "LIGHTRED_EX"))
+            self.player_won()
         else:
-            print(communicate("Dealer alive. My buddy!", "LIGHTRED_EX"))
+            print(communicate("Dealer alive!"))
+            play_audio()
             shuffle(self.cylinder)
 
     def __call__(self):
