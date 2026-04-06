04f15823a2383fab1e820357d5389950fffd8f59
Goga_ua <pavlenkoegor1412@gmail.com>
2026-04-06 09:59:47 +0200

    v3.0 beta

---
+++ BETA_Games/PredictTheShot/CODE/gui.py
@@ -0,0 +1,22 @@
+from system import *
+
+# SETTINGS
+
+tutorial_option = GUIOption("tutorial", system.get_tutorial)
+credits_option = GUIOption("credits", system.coming_soon)
+audio_option = GUIOption("audio", system.coming_soon)
+from_settings_to_mm = GUIOption("leave - main menu", system.pass_func)
+
+settings_options = (tutorial_option, credits_option, audio_option, from_settings_to_mm)
+settings_menu = Menu("settings", settings_options)
+
+# MAIN MENU
+
+play_option = GUIOption("play", system.coming_soon)
+settings_option = GUIOption("settings", settings_menu)
+quit_option = GUIOption("quit", system.pass_func)
+
+mm_options = (play_option, settings_option, quit_option)
+main_menu = Menu("main menu", mm_options)
+
+main_menu()
