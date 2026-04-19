eb0fb97c7dc5e84ba2daf84bc0fd45d4ce076b9b
Goga_ua <pavlenkoegor1412@gmail.com>
2026-04-19 22:03:48 +0200

    GC EXP 0.4.5: gui: logistics options and menu

--- BETA_Games/NEW/GLOBAL WARS/GC_LITE_EXP/CODE/gui.py
+++ BETA_Games/NEW/GLOBAL WARS/GC_LITE_EXP/CODE/gui.py
@@ -1,11 +1,20 @@
 from system import *
 
+# LOGISTICS
+economy_option = Option("economy", system.coming_soon)  # COMING SOON
+cargo_transport_option = Option("cargo transport", system.coming_soon)  # COMING SOON
+from_logistics_to_hq = Option("back - headquarter", system.coming_soon)  # COMING SOON
+
+logistics_options = (economy_option, cargo_transport_option, from_logistics_to_hq)
+logistics_menu = Menu("logistics", logistics_options)
+
 # ARMY
 soldiers_option = Option("soldiers", system.coming_soon)  # COMING SOON
-combat_transport = Option("combat transport", system.coming_soon)  # COMING SOON
+combat_transport_option = Option("combat transport", system.coming_soon)  # COMING SOON
+infrastructure_option = Option("infrastructure", system.coming_soon)  # COMING SOON
 from_army_to_hq = Option("back - headquarter", system.pass_func)
 
-army_options = (soldiers_option, combat_transport, from_army_to_hq)
+army_options = (soldiers_option, combat_transport_option, infrastructure_option, from_army_to_hq)
 army_menu = Menu("army", army_options)
 
 # OPERATIONS
@@ -21,7 +30,7 @@ operations_menu = Menu("operations", operations_options)
 # HEADQUARTER
 operations_option = Option("operations", operations_menu)
 army_option = Option("army", army_menu)
-logistics_option = Option("logistics", system.coming_soon)  # COMING SOON
+logistics_option = Option("logistics", logistics_menu)
 from_hq_to_battle_modes = Option("leave battle", system.pass_func)
 
 hq_options = (operations_option, army_option, logistics_option, from_hq_to_battle_modes)
@@ -37,9 +46,9 @@ combat_modes_menu = Menu("combat modes", combat_modes_options)
 # MAIN MENU
 combat_modes_option = Option("combat modes", combat_modes_menu)
 settings_option = Option("settings", system.coming_soon)  # COMING SOON
-quit_game_option = Option("quit", system.pass_func)
+quit_game = Option("quit", system.pass_func)
 
-main_menu_options = (combat_modes_option, settings_option, quit_game_option)
+main_menu_options = (combat_modes_option, settings_option, quit_game)
 main_menu = Menu("main menu", main_menu_options)
 
 main_menu()
