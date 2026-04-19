db20f85e7a0c34873ff2383eebce6c800c31ecfb
Goga_ua <pavlenkoegor1412@gmail.com>
2026-04-19 21:15:49 +0200

    GC EXP 0.3: combat modes, headquarter

--- BETA_Games/NEW/GLOBAL WARS/GC_LITE_EXP/gui.py
+++ BETA_Games/NEW/GLOBAL WARS/GC_LITE_EXP/gui.py
@@ -1,8 +1,24 @@
 from system import *
 
+# HEADQUARTER
+operations_option = Option("operations", system.coming_soon)  # COMING SOON
+army_option = Option("army", system.coming_soon)  # COMING SOON
+logistics_option = Option("logistics", system.coming_soon)  # COMING SOON
+from_hq_to_battle_modes = Option("leave battle", system.pass_func)
+
+hq_options = (operations_option, army_option, logistics_option, from_hq_to_battle_modes)
+hq_menu = Menu("headquarter", hq_options)
+
+# COMBAT MODES
+frontline_mode_option = Option("frontline", hq_menu)
+from_modes_to_mm = Option("back - main menu", system.pass_func)
+
+combat_modes_options = (frontline_mode_option, from_modes_to_mm)
+combat_modes_menu = Menu("combat modes", combat_modes_options)
+
 # MAIN MENU
-combat_modes_option = Option("combat modes", system.coming_soon)
-settings_option = Option("settings", system.coming_soon)
+combat_modes_option = Option("combat modes", combat_modes_menu)
+settings_option = Option("settings", system.coming_soon)  # COMING SOON
 quit_game_option = Option("quit", system.pass_func)
 
 main_menu_options = (combat_modes_option, settings_option, quit_game_option)
