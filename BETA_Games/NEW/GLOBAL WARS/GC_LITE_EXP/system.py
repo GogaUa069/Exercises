6c7993b5485918b04045d6d0b40403e4000a766c
Goga_ua <pavlenkoegor1412@gmail.com>
2026-04-19 20:47:14 +0200

    GC EXP 0.1

---
+++ BETA_Games/NEW/GLOBAL WARS/GC_LITE_EXP/system.py
@@ -0,0 +1,50 @@
+from colorama import Fore, Style
+
+
+class System:
+    def __init__(self):
+        self.INPUT = self.communicate("<<< ", "LIGHTWHITE_EX")
+        self.pass_func = lambda: None
+        self.coming_soon = lambda: print(self.communicate("COMING SOON...", "LIGHTWHITE_EX"))
+
+    @staticmethod
+    def communicate(text, color):
+        text = getattr(Fore, color) + text + Style.RESET_ALL if color is not None else text
+        return text
+
+
+system = System()
+
+
+class Menu:
+    def __init__(self, header, options):
+        self.HEADER = header.upper()
+        self.OPTIONS = options
+
+    def get_menu(self):
+        print(system.communicate(f"\n[ {self.HEADER} ]", "LIGHTRED_EX"))
+        for indx, option in enumerate(self.OPTIONS, 1):
+            print(system.communicate(f"{indx}. {option.NAME}", "GREEN"))
+
+    def _check_answer(self, answer):
+        for indx, option in enumerate(self.OPTIONS, 1):
+            if answer.upper() in (option.NAME.upper(), str(indx)):
+                option()
+                return
+        print(system.communicate("ERROR: Select one of the options shown above!", "LIGHTRED_EX"))
+
+    def __call__(self):
+        answer = str()
+        while answer != str(len(self.OPTIONS)):
+            self.get_menu()
+            answer = input(system.INPUT)
+            self._check_answer(answer)
+
+
+class Option:
+    def __init__(self, name, func):
+        self.NAME = name.upper()
+        self.FUNC = func
+
+    def __call__(self):
+        self.FUNC()
