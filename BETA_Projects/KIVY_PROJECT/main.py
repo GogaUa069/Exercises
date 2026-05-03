19496b8173fc85eb4f75edd8d6c9e5dd5617b6f2
Goga_ua <pavlenkoegor1412@gmail.com>
2026-05-03 14:17:09 +0200

    1.1

--- BETA_Projects/KIVY_PROJECT/main.py
+++ BETA_Projects/KIVY_PROJECT/main.py
@@ -1,20 +1,57 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.core.window import Window
+
+Window.maximize()
 
 
-
-class MainWidget(BoxLayout):
+class MainMenu(Screen):
 
     @staticmethod
-    def play_btn():
-        print("Clicked!")
+    def play():
+        pass
+
+    @staticmethod
+    def settings():
+        app = App.get_running_app()
+        app.root.transition.direction = "left"
+        app.root.current = "settings"
+
+    @staticmethod
+    def quit():
+        App.get_running_app().stop()
+
+
+class Settings(Screen):
+
+    @staticmethod
+    def tutorial():
+        pass
+
+    @staticmethod
+    def credits():
+        pass
+
+    @staticmethod
+    def audio():
+        pass
+
+    @staticmethod
+    def from_settings_to_mm():
+        app = App.get_running_app()
+        app.root.transition.direction = "right"
+        app.root.current = "main_menu"
 
 
 class MyApp(App):
+    title = "Predict the Shot 2D"
 
     @staticmethod
     def build():
-        return MainWidget()
+        sm = ScreenManager(transition=SlideTransition())
+        sm.add_widget(MainMenu(name="main_menu"))
+        sm.add_widget(Settings(name="settings"))
+        return sm
 
 
 if __name__ == "__main__":
