2ba067ffb29a64e8ae82f98f61f5fa9a37c962d2
Goga_ua <pavlenkoegor1412@gmail.com>
2026-05-18 22:22:30 +0200

    moved music and sounds to __init__ in MyApp

--- BETA_Projects/KIVY_PROJECT/CODE/main.py
+++ BETA_Projects/KIVY_PROJECT/CODE/main.py
@@ -9,9 +9,9 @@ def resource_path(path):
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.core.audio import SoundLoader
-from kivy.uix.button import Button
 
 Window.maximize()
 
@@ -80,9 +80,10 @@ class MyApp(App):
         super().__init__(**kwargs)
         self.assets_images = resource_path("../UTILS/IMAGES/")
 
-    def build(self):
         self.click_sound = SoundLoader.load(resource_path("../UTILS/AUDIO/SOUNDS/click.mp3"))
-        self.music = SoundLoader.load(resource_path("../UTILS/AUDIO/SOUNDTRACKS/background_soundtrack_main.wav"))
+        self.music = SoundLoader.load(resource_path("../UTILS/AUDIO/SOUNDTRACKS/mm_soundtrack1.wav"))
+
+    def build(self):
 
         if self.music:
             self.music.loop = True
