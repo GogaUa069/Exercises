2d19f68d3f77420167438956da9a124040267aad
Goga_ua <pavlenkoegor1412@gmail.com>
2026-07-26 15:21:37 +0200

    border 0.5

--- BETA_Games/NEW/TankWars/TW_5/border.py
+++ BETA_Games/NEW/TankWars/TW_5/border.py
@@ -14,7 +14,7 @@ class Cell:
         return self.nation if self.nation is None else self.nation.name
 
     def get_symbol(self):
-        return communicate(self.symbol, set_color(self.color))
+        return communicate(self.symbol, set_color(self.color)) if self.unit is None else self.unit.get_symbol()
 
     def __setattr__(self, key, value):
         if key == "unit" and value is not None:
@@ -25,15 +25,15 @@ class Cell:
 
     def __repr__(self):
         return (f"cell __repr__:\n"
-                f"- name: {self.name}\n"
+                f"- name:   {self.name}\n"
                 f"- symbol: {self.symbol}\n"
                 f"- nation: {self.get_nation()}\n"
-                f"- unit: {self.unit is not None}")
+                f"- unit:   {self.unit is not None}") if self.unit is None else self.unit.__repr__()
 
     def __str__(self):
         return communicate(f"CELL INFO:\n"
-                           f"- name: {self.name}\n"
-                           f"- nation: {self.get_nation()}")
+                           f"- name:   {self.name}\n"
+                           f"- nation: {self.get_nation()}") if self.unit is None else self.unit.__str__()
 
 
 class Border:
@@ -41,13 +41,43 @@ class Border:
         self.width = width
         self.height = height
         self.border = [[Cell() for _ in range(self.width)] for _ in range(self.height)]
-        ...
+        self.__set_territories()
+        self.__set_forest()
+
+    def __set_territories(self):
+        for row in self.border:
+            for cell in row[:self.width//2+1]:
+                cell.nation = player1.nation
+            for cell in row[self.width//2:]:
+                cell.nation = player2.nation
+
+    def __set_forest(self):
+        trees = self.width * self.height // 10
+        for _ in range(trees):
+            while True:
+                x = randint(1, self.width-1)
+                y = randint(1, self.height-1)
+                if self.border[y][x].name == "land":
+                    break
+            self.border[y][x] = Cell(name="tree", symbol="^", color=set_color("green"))
+
+    def get_border(self, player):
+        for row in self.border:
+            for cell in row:
+                print(cell.get_symbol() if cell.nation in (None, player.nation) else communicate("#", "WHITE"), end="")
+            print()
 
     def __getitem__(self, item):
-        ...
+        x, y = item
+        return self.border[y-1][x-1]
 
     def __setitem__(self, key, value):
-        ...
+        x, y = key
+        self.border[y-1][x-1].unit = value
 
     def __delitem__(self, key):
-        ...
+        x, y = key
+        self.border[y-1][x-1].unit = None
+
+
+border = Border(width=100, height=25)
