91a90b9245e9bee5ba89d4849a6e52decb9f760a
Goga_ua <pavlenkoegor1412@gmail.com>
2026-08-21 23:13:50 +0200

    v1.4.2

--- BETA_Games/NEW/NWJR/weapons.py
+++ BETA_Games/NEW/NWJR/weapons.py
@@ -12,33 +12,33 @@ class Weapon:
 
 
 class AssaultRifle(Weapon):
-    def __init__(self, team, name, mf, ammo):
+    def __init__(self, team, name, mf, rounds):
         super().__init__(weapon_type="Assault Rifle", team=team, name=name, manufacturer=mf)
-        self.ammo = ammo
+        self.rounds = rounds
 
 
-SCARL = AssaultRifle(team=HECU, name="SCAR-L CQC", mf=FN,      ammo=30)
-M16A4 = AssaultRifle(team=HECU, name="M16A4",      mf=Colt_FN, ammo=30)
-G36C = AssaultRifle( team=HECU, name="G36C",       mf=H_K,     ammo=30)
+SCARL = AssaultRifle(team=HECU, name="SCAR-L CQC", mf=FN,      rounds=30)
+M16A4 = AssaultRifle(team=HECU, name="M16A4",      mf=Colt_FN, rounds=30)
+G36C = AssaultRifle( team=HECU, name="G36C",       mf=H_K,     rounds=30)
 
-HK416 = AssaultRifle(    team=SWAT,  name="HK416 D10RS", mf=H_K,           ammo=30)
-DDM4V7 = AssaultRifle(   team=SWAT,  name="DDM4 V7",     mf=DanielDefense, ammo=32)
-MCXVirtus = AssaultRifle(team=SWAT,  name="MCX Virtus",  mf=SIG,           ammo=30)
+HK416 = AssaultRifle(    team=SWAT,  name="HK416 D10RS", mf=H_K,           rounds=30)
+DDM4V7 = AssaultRifle(   team=SWAT,  name="DDM4 V7",     mf=DanielDefense, rounds=32)
+MCXVirtus = AssaultRifle(team=SWAT,  name="MCX Virtus",  mf=SIG,           rounds=30)
 
 
 class Pistol(Weapon):
-    def __init__(self, team, name, mf, ammo):
+    def __init__(self, team, name, mf, rounds):
         super().__init__(weapon_type="Pistol", team=team, name=name, manufacturer=mf)
-        self.ammo = ammo
+        self.rounds = rounds
 
 
-M9A3 = Pistol(       team=HECU, name="M9A3",                  mf=Beretta, ammo=17)
-DesertEagle = Pistol(team=HECU, name="Desert Eagle Mark XIX", mf=IWI,     ammo=7)
-FSMk2 = Pistol(      team=HECU, name="Five-SeveN Mk2",        mf=FN,      ammo=20)
+M9A3 = Pistol(       team=HECU, name="M9A3",                  mf=Beretta, rounds=17)
+DesertEagle = Pistol(team=HECU, name="Desert Eagle Mark XIX", mf=IWI,     rounds=7)
+FSMk2 = Pistol(      team=HECU, name="Five-SeveN Mk2",        mf=FN,      rounds=20)
 
-P226 = Pistol(      team=SWAT, name="P226",          mf=SIG,   ammo=20)
-Glock17 = Pistol(   team=SWAT, name="Glock 17 Gen5", mf=Glock, ammo=17)
-USPCompact = Pistol(team=SWAT, name="USP Compact",   mf=H_K,   ammo=13)
+P226 = Pistol(      team=SWAT, name="P226",          mf=SIG,   rounds=20)
+Glock17 = Pistol(   team=SWAT, name="Glock 17 Gen5", mf=Glock, rounds=17)
+USPCompact = Pistol(team=SWAT, name="USP Compact",   mf=H_K,   rounds=13)
 
 
 class Knife(Weapon):
@@ -71,58 +71,58 @@ M26 = Grenade(team=SWAT, name="M26 Fragmentation Grenade", mf=USA)
 
 
 class Heavy(Weapon):
-    def __init__(self, team, name, mf, ammo, single_use=True):
+    def __init__(self, team, name, mf, rounds, single_use=True):
         super().__init__(weapon_type="Heavy", team=team, name=name, manufacturer=mf, single_use=single_use)
-        self.ammo = ammo
+        self.rounds = rounds
 
 
-M134 = Heavy(team=HECU, name="M134 Minigun",   mf=GE,  ammo=3000)
-NSV = Heavy( team=HECU, name="NSV 12.7mm HMG", mf=NSV, ammo=150)
+M134 = Heavy(team=HECU, name="M134 Minigun",   mf=GE,  rounds=3000)
+NSV = Heavy( team=HECU, name="NSV 12.7mm HMG", mf=NSV, rounds=150)
 
-M2HB = Heavy(team=SWAT, name="Browning M2HB",   mf=USA, ammo=100)
-DShK = Heavy(team=SWAT, name="DShK 12.7mm HMG", mf=DS,  ammo=50)
+M2HB = Heavy(team=SWAT, name="Browning M2HB",   mf=USA, rounds=100)
+DShK = Heavy(team=SWAT, name="DShK 12.7mm HMG", mf=DS,  rounds=50)
 
 
 class ERA(Weapon):
-    def __init__(self, team, name, mf, ammo, single_use=False):
+    def __init__(self, team, name, mf, rounds, single_use=False):
         super().__init__(weapon_type="ERA", team=team, name=name, manufacturer=mf, single_use=single_use)
-        self.ammo = ammo
+        self.rounds = rounds
 
 
-M72 = ERA(   team=HECU, name="M72 LAW",         mf=Nammo,    ammo=1, single_use=True)
-FGM148 = ERA(team=HECU, name="FGM-148 Javelin", mf=Raytheon, ammo=1)
-M4 = ERA(    team=HECU, name="Carl Gustaf M4",  mf=SBD,      ammo=1)
+M72 = ERA(   team=HECU, name="M72 LAW",         mf=Nammo,    rounds=1, single_use=True)
+FGM148 = ERA(team=HECU, name="FGM-148 Javelin", mf=Raytheon, rounds=1)
+M4 = ERA(    team=HECU, name="Carl Gustaf M4",  mf=SBD,      rounds=1)
 
-AT4 = ERA( team=SWAT, name="AT4",                         mf=SBD, ammo=84)
-M320 = ERA(team=SWAT, name="M320 Grenade Launcher",       mf=H_K, ammo=1)
-Mk19 = ERA(team=SWAT, name="Mk19 Mod 3 Grenade Launcher", mf=GD,  ammo=48)
+AT4 = ERA( team=SWAT, name="AT4",                         mf=SBD, rounds=84)
+M320 = ERA(team=SWAT, name="M320 Grenade Launcher",       mf=H_K, rounds=1)
+Mk19 = ERA(team=SWAT, name="Mk19 Mod 3 Grenade Launcher", mf=GD,  rounds=48)
 
 
 class SniperRifle(Weapon):
-    def __init__(self, team, name, mf, ammo):
+    def __init__(self, team, name, mf, rounds):
         super().__init__(weapon_type="Sniper Rifle", team=team, name=name, manufacturer=mf)
-        self.ammo = ammo
+        self.rounds = rounds
 
 
-M40A1 = SniperRifle(  team=HECU, name="M40A1",     mf=Remington, ammo=5)
-Hecate2 = SniperRifle(team=HECU, name="Hecate II", mf=PGM,       ammo=7)
-Scout = SniperRifle(  team=HECU, name="Scout",     mf=Steyr,     ammo=10)
+M40A1 = SniperRifle(  team=HECU, name="M40A1",     mf=Remington, rounds=5)
+Hecate2 = SniperRifle(team=HECU, name="Hecate II", mf=PGM,       rounds=7)
+Scout = SniperRifle(  team=HECU, name="Scout",     mf=Steyr,     rounds=10)
 
-Remington700 = SniperRifle(team=SWAT, name="Remington 700", mf=Remington, ammo=5)
-AXMC = SniperRifle(        team=SWAT, name="AXMC",          mf=AI,        ammo=10)
-MRAD = SniperRifle(        team=SWAT, name="MRAD",          mf=BF,        ammo=10)
+Remington700 = SniperRifle(team=SWAT, name="Remington 700", mf=Remington, rounds=5)
+AXMC = SniperRifle(        team=SWAT, name="AXMC",          mf=AI,        rounds=10)
+MRAD = SniperRifle(        team=SWAT, name="MRAD",          mf=BF,        rounds=10)
 
 
 class Shotgun(Weapon):
-    def __init__(self, team, name, mf, ammo):
+    def __init__(self, team, name, mf, rounds):
         super().__init__(weapon_type="Shotgun", team=team, name=name, manufacturer=mf)
-        self.ammo = ammo
+        self.rounds = rounds
 
 
-SPAS12 = Shotgun(     team=HECU, name="SPAS-12",      mf=Franchi,    ammo=8)
-SXP = Shotgun(        team=HECU, name="SXP Defender", mf=Winchester, ammo=8)
-Mossberg590 = Shotgun(team=HECU, name="590A1",        mf=Mossberg,   ammo=9)
+SPAS12 = Shotgun(     team=HECU, name="SPAS-12",      mf=Franchi,    rounds=8)
+SXP = Shotgun(        team=HECU, name="SXP Defender", mf=Winchester, rounds=8)
+Mossberg590 = Shotgun(team=HECU, name="590A1",        mf=Mossberg,   rounds=9)
 
-M4Super90 = Shotgun(   team=SWAT, name="M4 Super 90",       mf=Benelli,   ammo=6)
-A300Patrol = Shotgun(  team=SWAT, name="A300 Patrol",       mf=Beretta,   ammo=8)
-Remington870 = Shotgun(team=SWAT, name="870 Police Magnum", mf=Remington, ammo=12)
+M4Super90 = Shotgun(   team=SWAT, name="M4 Super 90",       mf=Benelli,   rounds=6)
+A300Patrol = Shotgun(  team=SWAT, name="A300 Patrol",       mf=Beretta,   rounds=8)
+Remington870 = Shotgun(team=SWAT, name="870 Police Magnum", mf=Remington, rounds=12)
